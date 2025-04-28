8e4499277511b131f30e74be0214ae932f83f5bc
Patrick Strakeljahn <patrick.strakeljahn@gmail.com>
2025-04-28 22:44:54 +0200

    PSORM-23.3: prevent crash if criticval error occured

--- lib/packages/chatbot/src/handler/ChatServer.php
+++ lib/packages/chatbot/src/handler/ChatServer.php
@@ -54,88 +54,92 @@ class ChatServer
      */
     private function handleMessage(TcpConnection $connection, $data): void
     {
-        /** @var array{ token?: string, message?: string, type?: string }|null $arrData */
-        $arrData = json_decode($data, true);
-        if (!is_array($arrData) || !isset($arrData['type'], $arrData['token'])) {
-            $connection->send(json_encode([
-                "message" => '',
-                "inProgress" => false,
-                "error" => "Invalid request format",
-                "code" => 400
-            ]));
-            $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR]: Invalid request format", true);
-            return;
-        }
-        $arrUser = BearerToken::decodeToken($arrData['token'], true);
-        switch ($arrData['type']) {
-            case 'LOGIN':
-                if ($arrUser === null) {
-                    $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR]: No token provided.", true);
-                    return;
-                }
-                if ($arrUser['exp'] < time()) {
-                    $connection->send([
-                        "message" => '',
-                        "inProgress" => false,
-                        "error" => "Token expired!",
-                        "code" => 401
-                    ]);
-                    $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [SEND - UserID {$arrUser['UserID']}]: Token expired!", true);
-                } else {
-                    $initContext = "You are a friendly chatbot. Use a few emojis to lighten the atmosphere, but not too many. Not every message has to have an emoji. Introduce yourself briefly and greet the user by their first name in your first message. Only use information that you really know, e.g. through the context. Don't make up any additional information. If in doubt, be honest and say that you don't know. Use the german language by default, unless the user requests a different language.";
-                    $user = UserPeer::findById($arrUser['UserID']);
-                    if (!isset($this->sessions[$arrUser['UserID']])) {
-                        $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [SEND - UserID {$arrUser['UserID']}]: ChatSession created", true);
-                        $session = AiManager::getAiChatInstance(AiManager::PROVIDER_GEMINI, $initContext);
-                        $session->addUserContext($user);
-                        $callback = function ($chunk) use ($connection, $arrUser) {
-                            $connection->send([
-                                ...$chunk,
-                                "error" => null,
-                                "code" => 200
-                            ]);
-                            $output = str_replace("\n", "", $chunk["message"]);
-                            CliOutputHelper::output("← {$connection->getRemoteAddress()} - [AI RESPONSE - UserID {$arrUser['UserID']}]: '{$output}'");
-                        };
-                        $session->send($callback);
-                        $this->sessions[$arrUser['UserID']] = $session;
-                    } else {
-                        $conv = $this->sessions[$arrUser['UserID']]->getConversation();
-                        $connection->send([
-                            "message" => json_encode($conv),
-                            "inProgress" => false,
-                            "error" => null,
-                            "code" => 418
-                        ]);
+        try {
+            /** @var array{ token?: string, message?: string, type?: string }|null $arrData */
+            $arrData = json_decode($data, true);
+            if (!is_array($arrData) || !isset($arrData['type'], $arrData['token'])) {
+                $connection->send(json_encode([
+                    "message" => '',
+                    "inProgress" => false,
+                    "error" => "Invalid request format",
+                    "code" => 400
+                ]));
+                $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR]: Invalid request format", true);
+                return;
+            }
+            $arrUser = BearerToken::decodeToken($arrData['token'], true);
+            switch ($arrData['type']) {
+                case 'LOGIN':
+                    if ($arrUser === null) {
+                        $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR]: No token provided.", true);
+                        return;
                     }
-                }
-                return;
-            case 'USER':
-                if (!isset($this->sessions[$arrUser['UserID']])) {
-                    $connection->send(json_encode([
-                        "message" => '',
-                        "inProgress" => false,
-                        "error" => "Session not found. Please login first.",
-                        "code" => 401
-                    ]));
-                    $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR - UserID {$arrUser['UserID']}]: Session not found", true);
+                    if ($arrUser['exp'] < time()) {
+                        $connection->send([
+                            "message" => '',
+                            "inProgress" => false,
+                            "error" => "Token expired!",
+                            "code" => 401
+                        ]);
+                        $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [SEND - UserID {$arrUser['UserID']}]: Token expired!", true);
+                    } else {
+                        $initContext = "You are a friendly chatbot. Use a few emojis to lighten the atmosphere, but not too many. Not every message has to have an emoji. Introduce yourself briefly and greet the user by their first name in your first message. Only use information that you really know, e.g. through the context. Don't make up any additional information. If in doubt, be honest and say that you don't know. Use the german language by default, unless the user requests a different language.";
+                        $user = UserPeer::findById($arrUser['UserID']);
+                        if (!isset($this->sessions[$arrUser['UserID']])) {
+                            $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [SEND - UserID {$arrUser['UserID']}]: ChatSession created", true);
+                            $session = AiManager::getAiChatInstance(AiManager::PROVIDER_GEMINI, $initContext);
+                            $session->addUserContext($user);
+                            $callback = function ($chunk) use ($connection, $arrUser) {
+                                $connection->send([
+                                    ...$chunk,
+                                    "error" => null,
+                                    "code" => 200
+                                ]);
+                                $output = str_replace("\n", "", $chunk["message"]);
+                                CliOutputHelper::output("← {$connection->getRemoteAddress()} - [AI RESPONSE - UserID {$arrUser['UserID']}]: '{$output}'");
+                            };
+                            $session->send($callback);
+                            $this->sessions[$arrUser['UserID']] = $session;
+                        } else {
+                            $conv = $this->sessions[$arrUser['UserID']]->getConversation();
+                            $connection->send([
+                                "message" => json_encode($conv),
+                                "inProgress" => false,
+                                "error" => null,
+                                "code" => 418
+                            ]);
+                        }
+                    }
                     return;
-                }
-                $session = $this->sessions[$arrUser['UserID']];
-                $query = $arrData['message'] ?? '';
-                CliOutputHelper::output("→ {$connection->getRemoteAddress()} - [USER INPUT - UserID {$arrUser['UserID']}]: '{$query}'");
-                $session->addUserMessage($query);
-                $callback = function ($chunk) use ($connection, $arrUser) {
-                    $connection->send([
-                        ...$chunk,
-                        "error" => null,
-                        "code" => 200
-                    ]);
-                    $output = str_replace("\n", "", $chunk["message"]);
-                    CliOutputHelper::output("← {$connection->getRemoteAddress()} - [AI RESPONSE - UserID {$arrUser['UserID']}]: '{$output}'");
-                };
-                $session->send($callback);
-                return;
+                case 'USER':
+                    if (!isset($this->sessions[$arrUser['UserID']])) {
+                        $connection->send(json_encode([
+                            "message" => '',
+                            "inProgress" => false,
+                            "error" => "Session not found. Please login first.",
+                            "code" => 401
+                        ]));
+                        $this->logInstance->add(Logging::LOG_TYPE_CHATBOT, "→ {$connection->getRemoteAddress()} - [ERROR - UserID {$arrUser['UserID']}]: Session not found", true);
+                        return;
+                    }
+                    $session = $this->sessions[$arrUser['UserID']];
+                    $query = $arrData['message'] ?? '';
+                    CliOutputHelper::output("→ {$connection->getRemoteAddress()} - [USER INPUT - UserID {$arrUser['UserID']}]: '{$query}'");
+                    $session->addUserMessage($query);
+                    $callback = function ($chunk) use ($connection, $arrUser) {
+                        $connection->send([
+                            ...$chunk,
+                            "error" => null,
+                            "code" => 200
+                        ]);
+                        $output = str_replace("\n", "", $chunk["message"]);
+                        CliOutputHelper::output("← {$connection->getRemoteAddress()} - [AI RESPONSE - UserID {$arrUser['UserID']}]: '{$output}'");
+                    };
+                    $session->send($callback);
+                    return;
+            }
+        } catch (\Exception $e) {
+            CliOutputHelper::output("→ {$connection->getRemoteAddress()} - [EXCEPTION]: " . $e->getMessage(), true);
         }
     }
 
